1bdcfd2a27087264246f17d5c02be3715b4de5be
VeriCorp Dev <scharonvargas@gmail.com>
2025-12-15 13:51:46 -0300

    chore: increase vite chunkSizeWarningLimit to 2MB

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react()],
+    build: {
+      chunkSizeWarningLimit: 2000 // Suppress warning for large chunks (in kB)
+    },
     define: {
       // Garante retrocompatibilidade com código que usa process.env
       'process.env': env
